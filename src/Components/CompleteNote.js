441da7ed9b48f5313d7d3ac0bae1c7b271e38ffe
KrzGas <kgasik@gmail.com>
2019-05-28 16:38:20 +0200

    App v2 - do zrobienia zapisanie notatki w local storage

--- src/Components/CompleteNote.js
+++ src/Components/CompleteNote.js
@@ -1,3 +1,47 @@
 import React, { Component } from "react";
 
-export const CompleteNote = () => <div>Complete note</div>;
+class CompleteNote extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        notes: JSON.parse(localStorage.getItem("data"))
+      };
+    }
+  
+    deleteNote = id => {
+      const arrToRemove = this.state.notes.filter(note => note.id != id);
+  
+      this.setState({
+        notes: arrToRemove
+      });
+  
+      localStorage.setItem("data", JSON.stringify(arrToRemove));
+    };
+  
+    render() {
+      return (
+        <ul>
+          {this.state.notes.map((el, index) => {
+            return (
+              <li key={index}>
+                <div className="pending__note__container">
+                  <div className="pending">
+                    <input type="text" defaultValue ={el.title} id="titleNote"/>
+                    <input defaultValue ={el.description} id="descriptionNote"/>
+                    <span>{el.date}</span>
+                    <div className="pending__note_wrapper">
+                      <button onClick={() => this.deleteNote(el.id)}>
+                        Delete
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      );
+    }
+  }
+
+  export {CompleteNote};
--- src/Components/PendingNote.js
+++ src/Components/PendingNote.js
@@ -6,11 +6,11 @@ class PendingNote extends Component {
     this.state = {
       notes: JSON.parse(localStorage.getItem("data")),
       edit: false,
-      readOnly: true
+      isClick: false
     };
   }
 
-  editNote = (id) => {
+  editNote = id => {
     this.setState(prevState => {
       const arrId = this.state.notes.filter(note => note.id == id);
       // let update = JSON.parse(JSON.stringify(this.state.notes))
@@ -19,8 +19,16 @@ class PendingNote extends Component {
         edit: !prevState.edit,
         readOnly: !prevState.readOnly
       };
-    })
-}
+    });
+  };
+
+  completeNote = () => {
+    this.setState(prevState=>{
+      return {
+        isClick: !prevState.isClick
+      }
+    });
+  };
 
   deleteNote = id => {
     const arrToRemove = this.state.notes.filter(note => note.id != id);
@@ -33,19 +41,30 @@ class PendingNote extends Component {
   };
 
   render() {
-    //console.log(this.state.notes);
     return (
       <ul>
         {this.state.notes.map((el, index) => {
           return (
             <li key={index}>
               <div className="pending__note__container">
+              {this.state.isClick ? null :
                 <div className="pending">
-                  <input type="text" defaultValue ={el.title} id="titleNote" readOnly={this.state.readOnly} />
-                  <input defaultValue ={el.description} id="descriptionNote" readOnly={this.state.readOnly} />
+                  <input
+                    type="text"
+                    defaultValue={el.title}
+                    id="titleNote"
+                    readOnly={this.state.readOnly}
+                  />
+                  <input
+                    defaultValue={el.description}
+                    id="descriptionNote"
+                    readOnly={this.state.readOnly}
+                  />
                   <span>{el.date}</span>
                   <div className="pending__note_wrapper">
-                    <button>Complete</button>
+                    <button onClick={() => this.completeNote()}>
+                      Complete
+                    </button>
                     <button onClick={() => this.editNote(el.id)}>
                       {this.state.edit ? "Finish" : "Edit"}
                     </button>
@@ -53,7 +72,7 @@ class PendingNote extends Component {
                       Delete
                     </button>
                   </div>
-                </div>
+                </div>}
               </div>
             </li>
           );
